26e47d8d4d75bc3fc479e67cc9bc4377b27538ca
weldonyang <51804539+weldonyang@users.noreply.github.com>
2021-01-12 18:55:08 -0800

    Compared LR and PR models
    - Trained PR model
    - Visualized LR model
    - Visualized PR model with 4 degrees
    - Visualized PR model with 4 degrees but increased the # of inputs to make the graph smoother

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -17,39 +17,38 @@ lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)                  # transforms OG features into the correct matrix of degrees 
+lin_reg_2 = LinearRegression()                      # creating instance of LinearRegression()
+lin_reg_2.fit(X_poly, y)                            # lin_reg.fit(X_poly, y) is now a PR model with degree = 4 
 
 # Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
+plt.scatter(X, y, color='red')                      
+plt.plot(X, lin_reg.predict(X), color='blue')    
+plt.title('Salary vs Position Level (LR)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Polynomial Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
+plt.scatter(X, y, color='red')                    
+plt.plot(X, lin_reg_2.predict(X_poly), color='blue')            
+plt.title('Salary vs Position Level (PR)')
+plt.xlabel('Postion Level')
 plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
+X_grid = np.arange(min(X), max(X), 0.1)                                             # returns an array of features with increment of .1 instead of 1, increasing the # of data points                           
+X_grid = X_grid.reshape((len(X_grid), 1))                                           # returns a nx1 matrix of X_grid 
+plt.scatter(X, y, color='red')                       
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')   # transforms X_grid into the correct matrix of degrees 
+plt.title('Salary vs Position Level (PR)')
+plt.xlabel('Postion Level')
 plt.ylabel('Salary')
 plt.show()
 
 # Predicting a new result with Linear Regression
-lin_reg.predict([[6.5]])
+
 
 # Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
